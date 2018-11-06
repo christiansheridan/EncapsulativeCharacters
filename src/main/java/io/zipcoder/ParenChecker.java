734af902cc18ccb8782b2e93b27bb5b1d4341bba
Christian Sheridan <christiansheridan@zipcoders-MacBook-Pro-13.local>
2018-11-05 20:08:06 -0500

    Paren checker completed

--- src/main/java/io/zipcoder/ParenChecker.java
+++ src/main/java/io/zipcoder/ParenChecker.java
@@ -1,4 +1,110 @@
 package io.zipcoder;
 
+import java.util.Stack;
+
 public class ParenChecker {
+
+    public static boolean isParenMatch(String str) {
+        if (str.isEmpty()){
+            return true;
+        }
+
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < str.length(); i++) {
+            char current = str.charAt(i);
+            if (current == '(' || current == '[' || current == '{' || current == '<' || current == '"' || current == '\'') {
+                stack.push(current);
+            }
+
+            if (current == ')' || current == ']' || current == '}' || current == '>' || current == '"' || current == '\'') {
+                if (stack.isEmpty()) {
+                    return false;
+                    }
+
+                    char last = stack.peek();
+                    if (current == ')' && last == '(' || current == ']' && last == '[' || current == '}' && last == '{' || current == '>' && last == '<' || current == '"' && last == '"' || current == '\'' && last == '\'') {
+                        stack.pop();
+                    } else return false;
+                }
+            } return stack.isEmpty();
+    }
 }
+
+
+
+
+
+
+
+//for (int i = 0; i < str.length(); i++) {
+//        c = str.charAt(i);
+//        if (c == '(') {
+//        stack.push(c);
+//        }   if (c == '{') {
+//        stack.push('c');
+//        }   if (c == '[') {
+//        stack.push('c');
+//        }   if (c == '<') {
+//        stack.push('c');
+//        }   if (c == '"') {
+//        stack.push('c');
+//        }   if (c == '\'') {
+//        stack.push('c');
+//        }
+//        else if (c == ')') {
+//        if (stack.empty()) {
+//        return false;
+//        } else if (stack.peek() == '(') {
+//        return true;
+//        } else return false;
+//        }
+//        else if(c =='}'){
+//        if(stack.empty()){
+//        return false;
+//        }
+//        else if(stack.peek() == '{'){
+//        return true;
+//        }
+//        else return false;
+//
+//        }
+//        else if(c ==']'){
+//        if(stack.empty()){
+//        return false;
+//        }
+//        else if(stack.peek() == ']'){
+//        return true;
+//        }
+//        else return false;
+//
+//        }
+//        else if(c =='>'){
+//        if(stack.empty()){
+//        return false;
+//        }
+//        else if(stack.peek() == '<'){
+//        return true;
+//        }
+//        else return false;
+//
+//        }
+//        else if(c =='"'){
+//        if(stack.empty()){
+//        return false;
+//        }
+//        else if(stack.peek() == '"'){
+//        return true;
+//        }
+//        else return false;
+//        }
+//        else if(c =='\''){
+//        if(stack.empty()){
+//        return false;
+//        }
+//        else if(stack.peek() == '\''){
+//        return true;
+//        }
+//        else return false;
+//        }
+//        else return false;
+//        } return true;
--- src/test/java/io/zipcoder/ParenCheckerTest.java
+++ src/test/java/io/zipcoder/ParenCheckerTest.java
@@ -5,4 +5,80 @@ import org.junit.Test;
 
 public class ParenCheckerTest {
 
+    @Test
+    public void parensTest(){
+        //when
+        String testString = "(lets try this)";
+        boolean test = ParenChecker.isParenMatch(testString);
+        System.out.println(test);
+
+        Assert.assertTrue(test);
+
+    }
+
+    @Test
+    public void bracketTest(){
+        String testString = "[tax]";
+        boolean test = ParenChecker.isParenMatch(testString);
+
+        Assert.assertTrue(test);
+    }
+
+    @Test
+    public void bracesTest(){
+        String testString = "{smile}";
+        boolean test = ParenChecker.isParenMatch(testString);
+
+        Assert.assertTrue(test);
+    }
+
+    @Test
+    public void arrowTest(){
+        String testString = "<iamgreaterthanandlessthan>";
+        boolean test = ParenChecker.isParenMatch(testString);
+
+        Assert.assertTrue(test);
+    }
+
+    @Test
+    public void quoteTest(){
+        String testString = "\"don't quote me boy cause I ain't said ****\"";
+        boolean test = ParenChecker.isParenMatch(testString);
+
+        Assert.assertTrue(test);
+    }
+
+    @Test
+    public void apostropheTest(){
+        String testString = "'shortened words'";
+        boolean test = ParenChecker.isParenMatch(testString);
+
+        Assert.assertTrue(test);
+    }
+
+    @Test
+    public void shouldntPass(){
+        String testString = "(])";
+        boolean test = ParenChecker.isParenMatch(testString);
+
+        Assert.assertTrue(!test);
+    }
+
+    @Test
+    public void emptyTest(){
+        String testString = "";
+        boolean test = ParenChecker.isParenMatch(testString);
+
+        Assert.assertTrue(test);
+    }
+
+    @Test
+    public void singleParenTest(){
+        String testString = ")";
+        boolean test = ParenChecker.isParenMatch(testString);
+
+        Assert.assertTrue(!test);
+    }
+
+
 }
